8eb4cad1ce31ffa90e9bc05e1670ec7a80f50cda
Harunobu Ishii <harunobuishii547@gmail.com>
2023-11-13 17:18:51 -0500

    Larger Prime

--- differential_privacy.py
+++ differential_privacy.py
@@ -5,7 +5,7 @@ from differential_privacy.preprocess import preprocess
 import pandas as pd
 
 if __name__ == "__main__":
-    p = pow(2, 61) - 1
+    p = pow(2, 127) - 1
     # https://media.githubusercontent.com/media/usnistgov/SDNist/main/nist%20diverse%20communities%20data%20excerpts/massachusetts/ma2019.csv
     df = pd.read_csv("ma2019.csv")
 
