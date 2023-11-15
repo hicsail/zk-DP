25435ae042934bf70bb15f61e4de55c3aa166dd4
Harunobu Ishii <harunobuishii547@gmail.com>
2023-11-15 10:47:37 -0500

    Modularized execution file

--- test_e2e.py
+++ test_e2e.py
@@ -1,10 +1,6 @@
 import unittest
-import pandas as pd
 from picozk import *
-from picozk.poseidon_hash import PoseidonHash
-from differential_privacy.add_noise import add_noise
-from differential_privacy.preprocess import preprocess
-from differential_privacy.laplase import gen_laplace_table
+from differential_privacy.execute import execute
 import pandas as pd
 
 
@@ -18,27 +14,7 @@ class TestPicoZKEndToEnd(unittest.TestCase):
         key = 1987034928369859712  # TODO: Make a proper key
 
         with PicoZKCompiler("picozk_test", field=[p], options=["ram"]):
-            # Replace negative values and N with ave.(excl. neg values)
-            preprocess(df)
-
-            # Secrefy Key
-            key = SecretInt(key)
-
-            # Create laplase table
-            table = gen_laplace_table(sensitivity=1, p=p)
-            zk_lap_table = ZKList(table)
-            poseidon_hash = PoseidonHash(p, alpha=17, input_rate=3)
-            _key = poseidon_hash.hash([key])
-
-            for col in cols:
-                print("\nWorking on", col)
-
-                # Commit data and key
-                poseidon_hash = PoseidonHash(p, alpha=17, input_rate=3)
-                hashed_df = poseidon_hash.hash(list(df[col]))
-
-                # Add noise to df
-                add_noise(df, col, p, hashed_df, key, zk_lap_table)
+            execute(df, key, p, cols)
 
 
 if __name__ == "__main__":
