4789463b9202dd408645a323a4752de8f8e513ce
Harunobu Ishii <harunobuishii547@gmail.com>
2023-11-17 18:37:24 -0500

    formatted

--- differential_privacy/des_module/demo.py
+++ differential_privacy/des_module/demo.py
@@ -39,23 +39,21 @@ n_list = [0, 0, 1, 0, 1, 1, 0, 1,
 assert len(n_list) == input_size
 
 p = pow(2, 127) - 1
-with PicoZKCompiler("irs/picozk_test", field=[p], options=["ram"]):
+# fmt: on
 
+with PicoZKCompiler("irs/picozk_test", field=[p], options=["ram"]):
     # Initiate DEC
     n_list = ZKList(n_list)
     bit_list_key = ZKList(bit_list_key)
     DES_inst = DES()
-
 
     # DEC encryption
     enc_val, enc_lis = DES_inst.encrypt(n_list, bit_list_key)
     assert len(enc_lis) == 64
     print(f"\nencrypted value :{val_of(enc_val)}")
 
-
     # DEC decryption
     dec_val, dec_list = DES_inst.decrypt(enc_lis, bit_list_key)
     print(f"\ninput value     :{input_val}")
     print(f"decrypted value :{val_of(dec_val)}")
     assert input_val == val_of(dec_val)
-    # fmt: on
