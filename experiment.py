f49985f14d42a89168ddab644ef56f61d08d88a3
Harunobu Ishii <harunobuishii547@gmail.com>
2023-11-20 15:44:43 -0500

    Key expansion modified

--- experiment.py
+++ experiment.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     p = pow(2, 127) - 1
     # https://media.githubusercontent.com/media/usnistgov/SDNist/main/nist%20diverse%20communities%20data%20excerpts/massachusetts/ma2019.csv
     df = pd.read_csv("ma2019.csv")
-    key = 1987034928369859712  # TODO: Make a proper key
+    key = 1987034928369859712
 
     size = len(df)
     interval = [0.1, 0.3, 0.5, 0.7, 1.0]
     sizes = [int(i * size) for i in interval]
     res_list = []
 
-    with PicoZKCompiler("irs/picozk_test", field=[p], options=["ram"]):
+    with PicoZKCompiler("irs/picozk_test", field=[p], options=["ram"]):  # TODO: Modify so that we can experiment both posiedon hash and 3DES
         # Replace negative values and N with ave.(excl. neg values)
         preprocess(df)
         # Secrefy Key
