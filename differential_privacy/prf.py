b9c88de53326b6b36d515587c55acc737d2966c7
Harunobu Ishii <harunobuishii547@gmail.com>
2023-11-21 17:09:56 -0500

    Non Field switching method added

--- differential_privacy/prf.py
+++ differential_privacy/prf.py
@@ -14,8 +14,34 @@ def get_beacon(p):
     print(" ", now, ":", beacon)
     return beacon
 
-
 class Poseidon_prf:
+    def __init__(self, keys, p):
+        self.p = p
+        self.keys = ZKList(keys)
+        self.poseidon_hash = PoseidonHash(p, alpha=17, input_rate=3)
+
+    def shrink_bits(self, seed_h, size):
+        x = seed_h.to_binary()
+        shift = len(x.wires) - size
+        shifted_x = x >> shift
+        return shifted_x.to_arithmetic()
+
+    def generate_seed(self):
+        seed = []
+        beacon = get_beacon(self.p)
+        for key in self.keys:
+            bi_num1 = key.to_binary()
+            seed.append((bi_num1 ^ beacon).to_arithmetic())
+        return seed
+
+    def run(self, i):
+        seed = self.generate_seed()
+
+        # Encryption
+        seed_h = self.poseidon_hash.hash(seed + [i])
+        return self.shrink_bits(seed_h, 13)
+
+class Poseidon_prf_no_fieldswicth:
     def __init__(self, keys, p):
         self.p = p
         self.keys = ZKList(keys)
--- test_e2e.py
+++ test_e2e.py
@@ -4,7 +4,7 @@ from picozk import *
 from picozk.poseidon_hash import PoseidonHash
 from differential_privacy.add_noise import add_noise
 from differential_privacy.preprocess import preprocess
-from differential_privacy.prf import TripleDES_prf, Poseidon_prf
+from differential_privacy.prf import TripleDES_prf, Poseidon_prf, Poseidon_prf_no_fieldswicth
 
 
 class TestPicoZKEndToEnd(unittest.TestCase):
@@ -31,6 +31,10 @@ class TestPicoZKEndToEnd(unittest.TestCase):
 
             prf_func = Poseidon_prf(keys, p)
             add_noise(sdf, p, hashed_df, prf_func)
+
+            prf_func = Poseidon_prf_no_fieldswicth(keys, p)
+            add_noise(sdf, p, hashed_df, prf_func)
+
 
 
 if __name__ == "__main__":
