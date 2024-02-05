2b341b20a8ce50be4ceab07267ea15eb3345830b
Harunobu Ishii <harunobuishii547@gmail.com>
2024-02-05 09:37:57 -0500

    Iteration added

--- differential_privacy/optimization.py
+++ differential_privacy/optimization.py
@@ -85,7 +85,6 @@ IL = [1057, 16122, 3315, 262, 15654]
 AK = [14540, 713, 15048, 10795, 8319]
 
 
-
 parent_node = [MA, NY, CA, IL, AK]
 US_Hist = [sum(values) for values in zip(*parent_node)]
 
@@ -99,20 +98,23 @@ l2_iter = 1000
 l1_rate = 0.01
 l1_iter = 1000
 
-# L2
 
-# Results
-H_star = L2_optimization(US_Hist, noised_H, l2_rate, l2_iter, init_sum)
-post_l2_Sum = sum(H_star)
-l2_loss = l2_obj_func(US_Hist, H_star)
+def optimization(US_Hist, noised_H, l1_rate, l1_iter, l2_rate, l2_iter, init_sum):
+
+    # L2
+    H_star = L2_optimization(US_Hist, noised_H, l2_rate, l2_iter, init_sum)
+    post_l2_Sum = sum(H_star)
+    l2_loss = l2_obj_func(US_Hist, H_star)
 
 
-# L1
-H_hat = [hs for hs in H_star]
-pre_l1_loss = l1_obj_func(H_hat, H_star)
-H_hat = L1_optimization(H_hat, H_star, l1_rate, l1_iter, post_l2_Sum)
-post_l1_sum = sum(H_hat)
-l1_loss = l1_obj_func(H_hat, H_star)
+    # L1
+    H_hat = [hs for hs in H_star]
+    pre_l1_loss = l1_obj_func(H_hat, H_star)
+    H_hat = L1_optimization(H_hat, H_star, l1_rate, l1_iter, post_l2_Sum)
+    post_l1_sum = sum(H_hat)
+    l1_loss = l1_obj_func(H_hat, H_star)
+
+    return l2_loss, post_l2_Sum, pre_l1_loss, post_l1_sum, l1_loss
 
 
 def generate_child(parent_node, parent_sum, l1_rate, l1_iter, l2_rate, l2_iter, parent_iter):
@@ -162,6 +164,9 @@ def generate_child(parent_node, parent_sum, l1_rate, l1_iter, l2_rate, l2_iter, 
             
 
     return parent_node, l2_loss_ttl, l1_loss_ttl
+
+
+l2_loss, post_l2_Sum, pre_l1_loss, post_l1_sum, l1_loss = optimization(US_Hist, noised_H, l1_rate, l1_iter, l2_rate, l2_iter, init_sum)
 
 parent_iter = 100
 parent_node, l2_loss_ttl, l1_loss_ttl = generate_child(parent_node, post_l1_sum, l1_rate, l1_iter, l2_rate, l2_iter, parent_iter)
